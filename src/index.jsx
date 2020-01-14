b5890be7b9adba10abaff343bb2c8342d6477605
John Riordan <john.riordan2@nhs.net>
2020-01-14 10:01:06 +0000

    added redux provider to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
+import rootReducer from './store/reducers/reducers-index';
 import MainContainerEnter from './components/main-container-enter';
 import MainContainerReconcile from './components/main-container-reconcile';
 import MainContainerRegtrans from './components/main-container-regtrans';
@@ -12,16 +15,21 @@ import MainContainerMap from './components/main-container-map';
 import './css/main.css';
 import './css/jfrzapple.css';
 
+const createdStore = applyMiddleware(thunk)(createStore);
+const store = createdStore(rootReducer);
+
 const Routed = (
-    <Router>
-        <div>
-            <Route exact path="/" component={MainContainerEnter} />
-            <Route path="/reconcile" component={MainContainerReconcile} />
-            <Route path="/regtrans" component={MainContainerRegtrans} />
-            <Route path="/mileage" component={MainContainerMileage} />
-            <Route path="/map" component={MainContainerMap} />
-        </div>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route exact path="/" component={MainContainerEnter} />
+                <Route path="/reconcile" component={MainContainerReconcile} />
+                <Route path="/regtrans" component={MainContainerRegtrans} />
+                <Route path="/mileage" component={MainContainerMileage} />
+                <Route path="/map" component={MainContainerMap} />
+            </div>
+        </Router>
+    </Provider>
 )
 
 ReactDOM.render(
